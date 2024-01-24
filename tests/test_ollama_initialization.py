289efb33f749d8041bd1efccd228d34d58e76bcf
Brian Lawrence <brian@briandlawrence.com>
2024-01-24 22:21:46 +0100

    test reorganize

--- tests/test_ollama_initialization.py
+++ tests/test_ollama_initialization.py
@@ -5,11 +5,3 @@ def test_llama_brain_initialization():
     """ Testing initialization of the brain """
     brain = LlamaBrain()
     assert isinstance(brain,LlamaBrain)
-
-def test_llama_thalamus_prompts():
-    """ Testing initialization of the brain """
-    brain = LlamaBrain()
-
-    thalamus_prompt = "You are part of a python program. Your only job is to review the following context and help the program redirect to the appropriate LLM. If the context contains a task that would require math or science, respond with the single word of 'LEFT', otherwise respond with a single word 'RIGHT' - ONLY RESPOND with one of those words, nothing else. Here is the context: "
-    context = "Here is a daily todo list: Apply to 5 jobs, respond to emails, call the kids, code in python, play guitar "
-    brain.think(thalamus_prompt + context)
