06863fe809316297cb031c6cb1c66cf1a643b1ca
Brian Lawrence <brian@briandlawrence.com>
2024-02-21 16:37:35 -0500

    Unit test refactoring fun

--- ai_agent/tests/test_llama_agent.py
+++ ai_agent/tests/test_llama_agent.py
@@ -1,11 +1,39 @@
-from langchain.prompts import PromptTemplate
-from ai_agent import LlamaAgent
+from unittest.mock import MagicMock
+from unittest.mock import patch
+import pytest
+from ai_agent.llama_agent import LlamaAgent
+# pylint: disable=redefined-outer-name
 
-def test_initialize_ok_with_mocked_dependencies():
-    model = 'test_model'
-    agent = LlamaAgent(model, TESTPROMPT)
-    assert isinstance(agent,LlamaAgent)
+MODEL = 'test_model'
+PROMPT = 'test_prompt'
 
-TESTPROMPT = PromptTemplate(
-        input_variables=["topic"],
-        template ="You are happy chatbot : {topic} ")
+@pytest.fixture
+def mock_llm_chain(mocker):
+    mock_chain_class = mocker.patch('ai_agent.llama_agent.LLMChain')
+    mock_chain_instance = MagicMock()
+    mock_chain_class.return_value = mock_chain_instance
+    return mock_chain_class, mock_chain_instance
+
+@patch('ai_agent.llama_agent.initialize_ollama')
+def test_initialize_the_agent(mock_initialize_ollama, mock_llm_chain):
+
+    LlamaAgent(MODEL, 'test_prompt')
+
+    mock_initialize_ollama.assert_called_once_with(MODEL)
+    mock_chain_class = mock_llm_chain[0]
+    mock_chain_class.assert_called_once_with(llm=mock_initialize_ollama.return_value, prompt=PROMPT, verbose=False)
+
+def test_request_with_mocked_dependencies(mock_llm_chain):
+
+    user_input = 'hello'
+    expected_response = 'hello world'
+    mock_llm_chain_instance = mock_llm_chain[1]
+    mock_llm_chain_instance.invoke.return_value = {"text": expected_response}
+
+    agent = LlamaAgent(MODEL, PROMPT)
+    response = agent.request(user_input)
+
+    # Assert
+    assert response == expected_response
+    mock_llm_chain_instance.invoke.assert_called_once_with(f"{user_input}")
+    assert agent.history == [user_input, expected_response], "Expected both input and response to be in history"
