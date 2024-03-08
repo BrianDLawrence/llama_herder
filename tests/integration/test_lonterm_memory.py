c0c7add8633e37aff4f9e07c13c460bc369cb06b
Brian Lawrence <brian@briandlawrence.com>
2024-03-08 10:53:17 -0500

    Integration test added to test persistent directory

--- tests/integration/test_lonterm_memory.py
+++ tests/integration/test_lonterm_memory.py
@@ -1,3 +1,4 @@
+import tempfile
 from longterm_memory import LlamaMemory
 
 def test_save_to_memory():
@@ -6,3 +7,15 @@ def test_save_to_memory():
     memory.remember_string(string_to_remember)
     vectorstore = memory.get_memory_as_vectorstore()
     assert string_to_remember in str(vectorstore.similarity_search("string"))
+
+def test_save_to_memory_persistent():
+    temppath = "/tests/longtermintegrationtest"
+    string_to_remember = "THIS IS A string to remember forever"
+
+    with tempfile.TemporaryDirectory() as temppath:
+        memory = LlamaMemory(temppath)
+        memory.remember_string(string_to_remember)
+
+        memory2 = LlamaMemory(temppath)
+        vectorstore = memory2.get_memory_as_vectorstore()
+        assert string_to_remember in str(vectorstore.similarity_search("string"))
