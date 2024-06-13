90060b3cd1bd562b09c17caecb96e6e86cf6bfd4
Brian Lawrence <brian@briandlawrence.com>
2024-06-13 13:50:35 -0700

    adding multiprocessing - will add additional listeners in near future.

--- run_herder.py
+++ run_herder.py
@@ -3,9 +3,19 @@ Main python program to run the herder
 This is a work in progress and not CLEAN at all
 But it will be :) 
 """
+import multiprocessing
 from message_handler import Handler
 from ai_agent import Agent
 from config import CLOUDAMQP_URL, GENERAL
+
+def start_listeners():
+    simple_action_listener = multiprocessing.Process(target=start_simple)
+    simple_action_listener.start()
+
+def start_simple():
+    handler = Handler(CLOUDAMQP_URL)
+    print("Starting simple action listener on channel hub ")
+    handler.listen("hub",callback)
 
 # pylint: disable=unused-argument
 def callback(ch, method, properties, body):
@@ -22,5 +32,4 @@ def callback(ch, method, properties, body):
 
 
 if __name__ == "__main__":
-    handler = Handler(CLOUDAMQP_URL)
-    handler.listen("hub",callback)
+    start_listeners()
